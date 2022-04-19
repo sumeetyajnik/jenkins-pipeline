f68ef9ebb681963243a5e66f9c2c940d5514d4a1
Sumeet Yajnik <sumeet.yajnik@ellucian.com>
2022-04-19 13:17:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Repository to go along with the following tutorials: 
+
+* *Building a Spring Boot application in Jenkins* on the [theme-park-job]
+* *Building a Spring Boot application in Docker and Jenkins* on the [theme-park-job-docker]
+* *Deploying a Spring Boot application into AWS with Jenkins* on the [theme-park-job-aws]
+
+## Running Jenkins
+
+`./gradlew docker dockerRun`
+
+Jenkins will then be available at [http://localhost:8080](http://localhost:8080).
---
+++ createJobs.groovy
@@ -0,0 +1,52 @@
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
+pipelineJob('theme-park-job-aws') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-aws')
+                }
+            }
+        }
+    }
+}
